0533126f40023977050d1904df304db6f209e501
Bastien <21700836@etu.unicaen.fr>
2019-03-20 10:09:25 +0100

    mise en fonctionnement

--- td4/app/routes/developers/edit.js
+++ td4/app/routes/developers/edit.js
@@ -4,7 +4,16 @@ import EmberObject from '@ember/object';
 export default Route.extend({
   templateName: 'developers/new',
   afterModel(model){
-    set(model,'copy',JSON.stringify(model));
+    let copy=EmberObject.create(model.toJSON());
+    set(model,'copy',copy);
     return model;
+  },
+  actions:{
+    save(model){
+      model.setProperties(JSON.parse(JSON.stringify(model.copy)));
+      model.save().then(
+        //display message
+      );
+    }
   }
 });
--- td4/app/routes/developers/new.js
+++ td4/app/routes/developers/new.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import EmberObject,{get,set} from '@ember/object';
+import EmberObject from '@ember/object';
 
 export default Route.extend({
   model(){
